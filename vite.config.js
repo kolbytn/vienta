92bbd578bf7d654c2fcc0027293313df7bcaa67a
Kolby Nottingham <kolbytn@gmail.com>
2025-02-19 11:38:05 -0800

    firebase init

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,23 @@ import { fileURLToPath } from "url";
 import react from "@vitejs/plugin-react";
 
 const path = fileURLToPath(import.meta.url);
+const root = join(dirname(path), "client");
 
 export default {
-  root: join(dirname(path), "client"),
+  root,
   plugins: [react()],
   publicDir: join(dirname(path), "public"),
+  build: {
+    outDir: join(dirname(path), "dist"),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        client: join(root, "entry-client.jsx"),
+        server: join(root, "entry-server.jsx")
+      }
+    }
+  },
+  ssr: {
+    noExternal: ['react-feather']
+  }
 };
